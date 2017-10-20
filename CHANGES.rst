d05b3cadcd40dd64f607e85256415f8dd3a5b122
Alex Ganose <alexganose@googlemail.com>
2017-10-20 13:55:27 +0100

    Prepare all files for v1 release

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+Change log
+==========
+
+v1.0.0
+-----------
+* Initial release of bapt
+* Fading, custom gradients, and other options supported
--- setup.py
+++ setup.py
@@ -14,26 +14,26 @@ setup(
     long_description="""
 Get yourself some nice band alignment diagrams
 """,
-    url="https://github.com/utf/bap",
+    url="https://github.com/utf/bapt",
     author="Alex Ganose",
-    author_email="Alex Ganose",
+    author_email="alexganose@googlemail.com",
     license='MIT',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.3'
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics'
         ],
     keywords='chemistry dft band alignment ionisation potential electron',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['matplotlib'],
     entry_points={'console_scripts': ['bapt = bapt.cli:main']}
 )
