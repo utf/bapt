e1d7ba78bcdef9a89c2d760d3b1be5186c835e0c
Seán Kavanagh <51478689+kavanase@users.noreply.github.com>
2020-01-12 10:47:11 +0000

    Import from bapt

--- bapt/cli.py
+++ bapt/cli.py
@@ -5,7 +5,7 @@
 import sys
 import argparse
 
-from __init__ import read_config, get_plot, get_plot_novac
+from bapt import read_config, get_plot, get_plot_novac
 
 __author__ = "Alex Ganose"
 __version__ = "0.1"
